8750ab64cf6a5d228d15cdc7fe0a6102340c520c
hidexir <hidexir@gmail.com>
2020-03-02 01:10:36 +0900

    wip

--- 157/c.py
+++ 157/c.py
@@ -4,13 +4,15 @@ n = list(map(int, input().split()))
 b = [list(map(int, input().split())) for i in range(n[1])]
 keta = n[0]
 ans = [0] * keta
-ans_tmp = [0] * keta
+ans[0] = 1
 
 
 for target_list in b:
     key = target_list[0]
     value = target_list[1]
-    if value == 0:
+    if key == 1:
+        ans[key - 1] = value
+    elif value == 0:
         ans[key-1] = value
     elif ans[key-1] == 0:
         ans[key-1] = value
@@ -18,10 +20,6 @@ for target_list in b:
         ans[key-1] = value
     else:
         pass
-
-if ans == ans_tmp:
-    ans[0] = 1
-
 
 for target_list in range(keta):
     if len(ans) == 1:
@@ -32,4 +30,5 @@ for target_list in range(keta):
     else:
         break
 
-print("".join(map(str, ans)))
+h = "".join(map(str, ans))
+print(h)
